c0d5c4dec91fb9cd11971c24a3fa2b4700bcec1e
bigrocs <bigrocs@qq.com>
2020-11-22 09:14:59 +0800

    自动提交 git 代码

---
+++ util/signature.go
@@ -0,0 +1,69 @@
+package handler
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/clbanning/mxj"
+)
+
+// SessionInfo 解密小程序会话加密信息
+func SessionInfo(EncryptedData, sessionKey, iv string) (info *mxj.Map, err error) {
+	// return mxj.NewMapJson([]byte(req.json))
+	cipherText, err := base64.StdEncoding.DecodeString(EncryptedData)
+	aesKey, err := base64.StdEncoding.DecodeString(sessionKey)
+	aesIv, err := base64.StdEncoding.DecodeString(iv)
+	if err != nil {
+		return
+	}
+	raw, err := AESDecryptData(cipherText, aesKey, aesIv)
+	if err != nil {
+		return
+	}
+	return mxj.NewMapJson(raw)
+}
+
+func AESDecryptData(cipherText []byte, aesKey []byte, iv []byte) (rawData []byte, err error) {
+
+	const (
+		BLOCK_SIZE = 32             // PKCS#7
+		BLOCK_MASK = BLOCK_SIZE - 1 // BLOCK_SIZE 为 2^n 时, 可以用 mask 获取针对 BLOCK_SIZE 的余数
+	)
+
+	if len(cipherText) < BLOCK_SIZE {
+		err = fmt.Errorf("the length of ciphertext too short: %d", len(cipherText))
+		return
+	}
+
+	plaintext := make([]byte, len(cipherText)) // len(plaintext) >= BLOCK_SIZE
+
+	// 解密
+	block, err := aes.NewCipher(aesKey)
+	if err != nil {
+		panic(err)
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(plaintext, cipherText)
+
+	// PKCS#7 去除补位
+	amountToPad := int(plaintext[len(plaintext)-1])
+	if amountToPad < 1 || amountToPad > BLOCK_SIZE {
+		err = fmt.Errorf("the amount to pad is incorrect: %d", amountToPad)
+		return
+	}
+	plaintext = plaintext[:len(plaintext)-amountToPad]
+
+	// 反拼接
+	// len(plaintext) == 16+4+len(rawXMLMsg)+len(appId)
+	if len(plaintext) <= 20 {
+		err = fmt.Errorf("plaintext too short, the length is %d", len(plaintext))
+		return
+	}
+
+	rawData = plaintext
+
+	return
+
+}
